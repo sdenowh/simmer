e9bf4763fe72bb8c6c7a9e2d1e829e1a1a34fdb2
Scott Denowh <sdenowh@comply365.com>
2025-07-10 22:40:05 -0500

    Initial Commit

---
+++ Simmer/SimmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimmerApp.swift
+//  Simmer
+//
+//  Created by Scott Denowh on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimmerTests/SimmerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimmerTests.swift
+//  SimmerTests
+//
+//  Created by Scott Denowh on 7/10/25.
+//
+
+import Testing
+@testable import Simmer
+
+struct SimmerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
